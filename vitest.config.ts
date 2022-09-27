ca8c09ecb0889193fa973317fcf815e7e97a1de8
dougkavanagh <dougkavanagh@gmail.com>
2022-09-27 17:18:25 -0400

    removed react ref

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
